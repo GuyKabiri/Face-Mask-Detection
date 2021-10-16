0f6f8465ac69262b0399f49db4ea86948cbe20db
Guy Kabiri <guy.kabiri@s.afeka.ac.il>
2021-10-16 13:15:43 +0300

    add polygon to the dictionary

--- COCOFaceMaskData.py
+++ COCOFaceMaskData.py
@@ -2,8 +2,10 @@ import os
 import sys
 # import torch
 import uuid
+import itertools
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from detectron2.structures import BoxMode
 
 '''
@@ -49,28 +51,27 @@ class FaceMaskData:
         self.masks_path = masks_path
         self.classes = [ 'with_mask', 'mask_weared_incorrect', 'without_mask' ]
 
-    # def split(self, train_size=.8, drop_rate=0, seed=42):
-    #     if train_size > 1 or train_size < 0:
-    #         raise ValueError('Split sizes can not be greater than 1 or less than 0')
+    def split(self, train_size=.8, drop_rate=0, seed=42):
+        if train_size > 1 or train_size < 0:
+            raise ValueError('Split sizes can not be greater than 1 or less than 0')
 
-    #     if drop_rate > 0:
-    #         self.images = self.images[:int(len(self.images) * (1 - drop_rate))]
-    #         self.masks = self.masks[:int(len(self.masks) * (1 - drop_rate))]
+        if drop_rate > 0:
+            self.records = self.records[:int(len(self.records) * (1 - drop_rate))]
 
-    #     if train_size == 1:
-    #         return self.images, self.masks
+        if train_size == 1:
+            return self.records
 
-    #     test_size = 1 - train_size
-    #     x_train, x_test, y_train, y_test = train_test_split(self.images, self.masks, test_size=test_size, random_state=seed)
+        test_size = 1 - train_size
+        train_records, test_records = train_test_split(self.records, test_size=test_size, random_state=seed)
 
-    #     return (x_train, y_train), (x_test, y_test)
+        return train_records, test_records
 
 
     def load_data(self, train_size=.8, drop_rate=0, seed=42):
 
         imgs_path = os.path.join(sys.path[0], self.images_path)
-        self.images = os.listdir(imgs_path)
-        self.masks = []
+        images = os.listdir(imgs_path)
+        self.records = []
 
         msks_path = self.masks_path
         if not self.masks_path:
@@ -80,7 +81,7 @@ class FaceMaskData:
 
         masks_df = pd.read_csv(msks_path)
 
-        for img in self.images:
+        for img in images:
             record = dict()
 
             record['file_name'] = '{}/{}'.format(imgs_path, img)
@@ -94,24 +95,32 @@ class FaceMaskData:
                 record['width'] = r['width']
                 record['height'] = r['height']
 
+                xmin, ymin, xmax, ymax = r['xmin'], r['ymin'], r['xmax'], r['ymax']
+
+                poly = [
+                        (xmin, ymin), (xmax, ymin),
+                        (xmax, ymax), (xmin, ymax)
+                ]
+                poly = list(itertools.chain.from_iterable(poly))
+
                 single_mask = {
                     'iscrowd':      int(len(temp_df) > 1),
                     # single_mask['bbox'] = [ r['xmin'], r['ymin'], r['boxW'], r['boxH'] ]
                     # single_mask['bbox_mode']= BoxMode.XYWH_ABS,
-                    'bbox':         [ r['xmin'], r['ymin'], r['xmax'], r['ymax'] ],
+                    'bbox':         [ xmin, ymin, xmax, ymax ],
                     'bbox_mode':    BoxMode.XYXY_ABS,
+                    'segmentation': [poly],
                     'category_id': r['class']
                 }
 
                 annotations.append(single_mask)
             
             record['annotations'] = annotations
-            self.masks.append(record)
+            self.records.append(record)
 
-        self.images = np.array(self.images)
-        self.masks = np.array(self.masks, dtype=object)
+        self.records = np.array(self.records, dtype=object)
 
-        return self.masks
+        return self.split()
 
 
 def test_data():
@@ -122,15 +131,30 @@ def test_data():
 
     from detectron2.data import DatasetCatalog, MetadataCatalog
 
-    DatasetCatalog.register('mask_dataset', lambda: faceMasksData.load_data())
-    MetadataCatalog.get('mask_dataset').set(thing_classes=faceMasksData.classes)
-    chess_metadata = MetadataCatalog.get('mask_dataset')
+    train_recs, valid_recs = faceMasksData.load_data()
+    records = {
+        'train':    train_recs,
+        'valid':    valid_recs
+    }
 
-    dataset_dicts = DatasetCatalog.get('mask_dataset')
+    for phase in ['train', 'valid']:
+        DatasetCatalog.register('masks_{}_dataset'.format(phase), lambda: records[phase] )
+        MetadataCatalog.get('masks_{}_dataset'.format(phase)).set(thing_classes=faceMasksData.classes)
 
-    print(len(dataset_dicts))
+    metadata_rec = {
+        'train':    MetadataCatalog.get('masks_train_dataset'),
+        'valid':    MetadataCatalog.get('masks_valid_dataset')
+    }
 
-    print(dataset_dicts[1])
+    dataset_dicts = {
+        'train':    DatasetCatalog.get('masks_train_dataset'),
+        'valid':    DatasetCatalog.get('masks_valid_dataset')
+    }
+
+    print('Training contains {} samples which is {:g}% of the data'.format(len(dataset_dicts['train']), len(dataset_dicts['train']) * 100 / (len(dataset_dicts['train']) + len(dataset_dicts['valid']))))
+    print('Validation contains {} samples which is {:g}% of the data'.format(len(dataset_dicts['valid']), len(dataset_dicts['valid']) * 100 / (len(dataset_dicts['train']) + len(dataset_dicts['valid']))))
+
+    print(dataset_dicts['train'][1])
 
 
     # (x_train, y_train), (x_test, y_test) = faceMasksData.load_data()
