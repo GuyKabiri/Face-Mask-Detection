e5e290a6e123500ac77fcf3a475d12cf09a4deb0
Guy Kabiri <guy.kabiri@s.afeka.ac.il>
2021-10-22 12:08:02 +0300

    add comments to data class

--- FaceMaskData.py
+++ FaceMaskData.py
@@ -1,46 +1,22 @@
 import os
-import sys
-import uuid
-# import torch
 import numpy as np
-import pandas as pd
 from sklearn.model_selection import train_test_split
 
-'''
-        file fields:
-        xmin,ymin,xmax,ymax,name,file,width,height,class,Xcent,Ycent,boxW,boxH
-
-        output:
-            x_train:    [img1.png, img2.png, img3.png, ...]
-            y_train:    [
-                            {   name:           img1.png,
-                                annotations:    [
-                                    {
-                                        xmin:   ... ,
-                                        ymin:   ... ,
-                                        xman:   ... ,
-                                        ymax:   ... ,
-                                        height: ... ,
-                                        width:  ... ,
-                                        class_name: with_mask   /   mask_weared_incorrect   /   without_mask ,
-                                        class_id:   0           /   1                       /   2 ,
-                                    }
-                                ]
-                            },
-                            {
-                                name:   ... ,
-                                annotations: [ { ... } ]
-                            }
-            ]
-
-'''
-
 class FaceMaskData:
-    def __init__(self, images_path, annotations_path):
+    def __init__(self, images_path, annotations_path):      #   saving the path of the images and the xml annotations
         self.images_path = images_path
         self.annotations_path = annotations_path
+        self.is_loaded = False
 
-    def split(self, train_size=.8, drop_rate=0, seed=42):
+
+    '''
+        Should not be used by the user. Call to `load_data` instead.
+    '''
+    def _split(self, train_size=.8, drop_rate=0, seed=42):
+
+        if not self.is_loaded:
+            raise RuntimeError('Split should not be used before `load_data`')
+
         if train_size > 1 or train_size < 0:
             raise ValueError('Split sizes can not be greater than 1 or less than 0')
 
@@ -57,12 +33,18 @@ class FaceMaskData:
         return (x_train, y_train), (x_test, y_test)
 
 
+    '''
+        Params:
+            -   train_size  -   percentage of the train data, the test size therfore will be 1-train_size.
+            -   drop_rate   -   percentage of amount of random data to drop, used for code testing.
+            -   seed        -   the seed to use for randomize the split of the data.
+    '''
     def load_data(self, train_size=.8, drop_rate=0, seed=42):
 
         self.images = np.array( [img for img in sorted(os.listdir(self.images_path))] )
         self.annotates = np.array( [ant for ant in sorted(os.listdir(self.annotations_path))] )
 
-        return self.split(train_size, drop_rate, seed)
+        return self._split(train_size, drop_rate, seed)
 
 
 def test_data():
