dea785ab0bae8ee9d927a54a9efbdc51e2113aa9
Guy Kabiri <guy.kabiri@s.afeka.ac.il>
2021-10-30 15:54:04 +0300

    change saving path

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -4,6 +4,10 @@ import pandas as pd
 from tqdm import tqdm
 import torchvision.ops as ops
 
+
+'''
+    apply Non-maximum Suppression by threshold for a single prediction (one image) and return the new predictions
+'''
 def get_pred_by_threshold(preds, threshold):
     keep = ops.nms(preds['boxes'], preds['scores'], threshold)
     
@@ -14,6 +18,10 @@ def get_pred_by_threshold(preds, threshold):
     
     return final_prediction
 
+
+'''
+    apply Non-maximum Suppression by threshold for batch (batch size can be equals to 1) and return new predictions
+'''
 def get_pred_by_threshold_batch(preds, threshold):
     
     for i in range(len(preds)):
@@ -22,6 +30,11 @@ def get_pred_by_threshold_batch(preds, threshold):
     return preds
 
 
+'''
+    run the entire dataloader in the model and save the output prediction in a dataframe
+    will the following columns for a single boundary box:
+        image_id, real_class, pred_class, score, iou
+'''
 def get_iou_as_df(model, loader, nms_thresh):
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     model = model.to(device).eval()
@@ -48,8 +61,8 @@ def get_iou_as_df(model, loader, nms_thresh):
                         'image_id':   target['image_id'][0],
                         'real_class': target['labels'][id_real_bbox],    # target['labels'][ious_ids[idx]]
                         'pred_class': output['labels'][idx],
-                        'iou':        ious.values[idx],
-                        'score':      output['scores'][idx],                
+                        'score':      output['scores'][idx],
+                        'iou':        ious.values[idx],                
                     }, ignore_index=True)
                     
                 
@@ -60,7 +73,7 @@ def get_iou_as_df(model, loader, nms_thresh):
             torch.cuda.empty_cache()
             progress_bar.update(1)
     
-    df = df.astype({ 'image_id': np.int32,
+    df = df.astype({ 'image_id':   np.int32,
                      'real_class': np.int32,
                      'pred_class': np.int32, 
                      'iou':        np.float32,
@@ -68,22 +81,31 @@ def get_iou_as_df(model, loader, nms_thresh):
     return df.sort_values('score', ascending=False, ignore_index=True)
 
 
-def calc_precision_recall(df, iou_thresh, eps=1e-6):
+'''
+    calculate recall and precision for the entire predicted data and return the modified dataframe
+'''
+def calc_precision_recall(df, iou_thresh, path, eps=1e-6):
+
     df['precision'] = 0.
     df['recall'] = 0.
+
+    #   evaluate TP/FP of each boundary box by specific IOU threshhold
     df['TPorFP'] = np.where((df['real_class'] == df['pred_class']) & (df['iou'] >= iou_thresh), True, False)
 
-    total_tp_cls = { c: len(df[(df['real_class']==c) & (df['TPorFP'])]) for c in df['real_class'].unique() }
-    count_cls_tp = { c: 0 for c in df['real_class'].unique() }
-    count_cls_instance = { c: 0 for c in df['real_class'].unique() }
+    total_tp_cls = { c: len(df[(df['real_class']==c) & (df['TPorFP'])]) for c in df['real_class'].unique() }    #   calculate the total TP for each class
+    count_cls_tp = { c: 0 for c in df['real_class'].unique() }          #   TP counter for each class as iterating over the data
+    count_cls_instance = { c: 0 for c in df['real_class'].unique() }    #   instances counter for each class
 
+    #   iterate over the dataframe rows
     for index, row in df.iterrows():
-        row_class = row['real_class']
-        count_cls_instance[row_class] += 1   # count instances of each class
+        row_class = row['real_class']       #   get the real class each boundary box referring to
+        count_cls_instance[row_class] += 1  #   count instances of each class
         if row['TPorFP']:
-            count_cls_tp[row_class] += 1     # count TP of each class
+            count_cls_tp[row_class] += 1    #   count TP of each class
+        
+        #   calculate precision and recall for each boundary box
         df.at[index, 'precision'] =  count_cls_tp[row_class] / count_cls_instance[row_class]
         df.at[index, 'recall'] = count_cls_tp[row_class] / (total_tp_cls[row_class] + eps)
 
-    df.to_csv('metrics/map_thresh_{:.3}.csv'.format(iou_thresh), index=False)
+    df.to_csv('{}/mAP@{:.3}.csv'.format(path, iou_thresh), index=False)
     return df
